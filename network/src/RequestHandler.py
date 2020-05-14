974a4b76c237167a1cececaa8caa93736db8f8c6
Basill-ini <portnikof@gmail.com>
2020-05-14 17:40:05 +0300

    ''

--- network/src/RequestHandler.py
+++ network/src/RequestHandler.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from http.server import BaseHTTPRequestHandler
+import json
 
 
 class RequestHandler(BaseHTTPRequestHandler):
-
     # the constructor method is not created since class
     # instances are created automatically by the server
 
@@ -20,9 +20,11 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
 
-        data_size = self.headers['Content-length']
-        print(data_size)
-        #data = self.rfile.read()
+        data_size = int(self.headers['Content-length'])
+
+        data = self.rfile.read(data_size)
+        data = data.decode('utf-8')
+        data = json.loads(data)
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
@@ -30,5 +32,3 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         answer = 'Все хорошо'.encode('utf-8')
         self.wfile.write(answer)
-
-
