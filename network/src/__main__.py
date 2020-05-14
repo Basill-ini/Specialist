f11c47e7800ff2688cea56573bdbe420a7bfc096
Basill-ini <portnikof@gmail.com>
2020-05-14 13:34:55 +0300

    Remove CmdParams

--- network/src/__main__.py
+++ network/src/__main__.py
@@ -6,10 +6,16 @@ from server import run_server
 from client import run_client
 
 
-parser = argparser.ArgumentParser(description='Networking')
-parser.add_argument('--server', dest='mode', action='store_const', const='server',
+parser = argparse.ArgumentParser(description='Networking')
+parser.add_argument('--server', dest='mode', action='store_const', const='run_server',
                     default=None, help='Run as server')
-parser.add_argument('--client', dest='mode', action='store_const', const='client',
+parser.add_argument('--client', dest='mode', action='store_const', const='run_client',
                     default=None, help='Run as client')
 
-run_server()
+ARGS = parser.parse_args()
+
+if ARGS.mode is not None:
+    ARGS.mode()
+else:
+    print('Unknown option')
+
