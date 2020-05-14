3c4c5f06605c0a1a3032fe109bfad69c6b8c6811
Basill-ini <portnikof@gmail.com>
2020-05-14 17:16:01 +0300

    ''

--- network/src/client.py
+++ network/src/client.py
@@ -2,8 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import json
+
 
 def run_client():
+    title = input('Наименование: ').strip()
+    price = input('Цена: ').strip()
+    if price == '':
+        price = 0
+    else:
+        price = int(price)
 
-    requests.get('http://192.168.1.69:8080/')
+    # pack object data into JSON
+    data = {
+        'title': title,
+        'price': price
+    }
+    json.dumps(data).encode('utf-8')
+
+    # post data to server
+    requests.post('http://192.168.1.69:8080/', data=data)
     print(response.text)
